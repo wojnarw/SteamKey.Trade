c7b620aa601a962f2ab5f2bc7ccb5976a0e9e2a8
Revadike <4411977+Revadike@users.noreply.github.com>
2025-06-22 19:00:58 +0200

    Refactor receiver handling in trade edit

--- pages/trade/new.vue
+++ pages/trade/new.vue
@@ -1,5 +1,29 @@
 <script setup>
+  import { isSteamID64 } from '~/assets/js/validate';
+
+  const supabase = useSupabaseClient();
   const { currentRoute } = useRouter();
+  const { User } = useORM();
+
+  const { data: resolvedPartnerId } = await useLazyAsyncData(`steamid-${currentRoute.query.partner}`, async () => {
+    if (currentRoute.query.partner && isSteamID64(currentRoute.query.partner)) {
+      const users = await User.query(supabase, [
+        {
+          filter: 'eq',
+          params: [User.fields.steamId, currentRoute.query.partner]
+        },
+        {
+          filter: 'limit',
+          params: [1]
+        }
+      ]);
+
+      if (users && users.length > 0) {
+        return users[0].id;
+      }
+    }
+    return currentRoute.query.partner;
+  });
 
   definePageMeta({
     middleware: 'authenticated'
@@ -10,7 +34,7 @@
   <trade-edit
     :copy-id="currentRoute.query.copy"
     :counter-id="currentRoute.query.counter"
-    :receiver="currentRoute.query.partner"
+    :receiver="resolvedPartnerId"
     :receiver-selected="currentRoute.query.receiverapps ? currentRoute.query.receiverapps.split(',').map(Number) : []"
     :sender-selected="currentRoute.query.senderapps ? currentRoute.query.senderapps.split(',').map(Number) : []"
   />
